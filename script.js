b20236e9a47efbb821bc3e1b79af05581fbc6994
Shade art <hamzabitmesra786@gmail.com>
2025-12-29 22:01:04 +0530

    added files

---
+++ script.js
@@ -0,0 +1,25 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+function nextSlide() {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}
+
+function prevSlide() {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+}
+
+
+setInterval(nextSlide, 5000);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") nextSlide();
+  if (e.key === "ArrowLeft") prevSlide();
+});
